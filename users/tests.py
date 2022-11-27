30734bf3b7afc1c6af793b9820df77792ffb0a47
Jumayev Ubaydullo <82611064+python019@users.noreply.github.com>
2022-11-27 15:17:26 +0500

    testing

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .views import get_random, get_access_token, get_refresh_token
 
-# Create your tests here.
+class TestGenericFunctions(APITestCase):
+
+    def test_get_random(self):
+        
+        rand1 = get_random(10)
+        rand2 = get_random(10)
+        rand3 = get_random(15)
+
+        self.assertTrue(rand1)
+
+        self.assertNotEqual(rand1, rand2)
+
+        self.assertEqual(len(rand1), 10)
+        self.assertEqual(len(rand3), 15)
+    
+    def test_get_access_token(self):
+        payload = {
+            "id": 1
+        }
+
+        token = get_access_token(payload)
+
+        self.assertTrue(token)
+    
+    def test_get_refresh_token(self):
+        token = get_refresh_token()
+
+        self.assertTrue(token)
